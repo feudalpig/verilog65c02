54310c0d63f5957c61bb533e735336c6684676f7
feudalpig <jpnooyen@gmail.com>
2025-07-27 12:13:42 -0400

    created blank top module

---
+++ sources/verilog65c02.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2025 12:11:56 PM
+// Design Name: 
+// Module Name: verilog65c02
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module verilog65c02(
+    input [1:0] a
+    );
+endmodule
