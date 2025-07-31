d0dc530c474e8087867a4bdcd7fae727f18f87c0
feudalpig <jpnooyen@gmail.com>
2025-07-31 16:02:32 -0400

    Added completed register module and sim file

---
+++ sources/register.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2025 12:42:32 PM
+// Design Name: 
+// Module Name: register
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register(
+    input CE, //clock enable
+    // OE (output enable) excluded as it is implemented
+    // in the data bus
+    input CLK,
+    input [7:0] IN,
+    output reg [7:0] OUT
+    );
+    
+    initial
+        OUT <= 8'd0;
+    
+    always @(negedge CLK)
+        if (CE)
+            OUT <= IN;
+    
+endmodule
---
+++ sources/register_sim.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/27/2025 12:50:08 PM
+// Design Name: 
+// Module Name: register_sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_sim(
+
+    );
+    
+    reg ce;
+    reg clk;
+    reg [7:0] in;
+    wire [7:0] out;
+    
+    integer i;
+    
+    register dut(.CE(ce), .CLK(clk), .IN(in), .OUT(out));
+    
+    always @(posedge clk)
+        ce = ~ce;
+    
+    initial
+    begin
+        ce = 0;
+        clk = 0;
+        for (i = 0; i < 1026; i = i + 1)
+        begin
+            in = i / 4;
+            #1 clk = ~clk;
+            if (i % 4 == 2)
+                //$display (out);
+                //$display (i/4);
+                if (out != i/4)
+                  $display ("Failure at", $time, i/4);
+        end
+    end
+    
+endmodule
